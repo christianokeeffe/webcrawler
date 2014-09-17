eb020fde3f32449ccdfa8302376ed3e9c9249f43
Christian OKeeffe <christian@joedal.dk>
2014-09-17 12:34:23 +0200

    added index table

---
+++ WebCrawler/incidenceVector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler
+{
+    class incidenceVector
+    {
+        public string term;
+        public List<int> pageIDs = new List<int>();
+
+        public incidenceVector (string term)
+        {
+            this.term = term;
+        }
+
+        public int pageCount
+        {
+            get { return pageIDs.Count(); }
+        }
+    }
+}
---
+++ WebCrawler/indexer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawler
+{
+    class indexer
+    {
+        public List<incidenceVector> getIndexTable(List<termPagePair> termPageList)
+        {
+            termPageList = sortPairs(termPageList);
+            return pairsToVectorTable(termPageList);
+        }
+
+        private List<termPagePair> sortPairs(List<termPagePair> listToSort)
+        {
+            listToSort.Sort(
+                delegate(termPagePair tp1, termPagePair tp2) 
+                { 
+                    int compval = tp1.term.CompareTo(tp1.term);
+                    if(compval == 0)
+                    {
+                        return tp1.pageID.CompareTo(tp2.pageID);
+                    }
+                    else
+                    {
+                        return compval;
+                    }
+                });
+            return listToSort;
+        }
+
+        private List<incidenceVector> pairsToVectorTable(List<termPagePair> inputList)
+        {
+            string currentTerm = "";
+            List<incidenceVector> returnList = new List<incidenceVector>();
+            for(int i = 0; i < inputList.Count; i++)
+            {
+                if(currentTerm != inputList[i].term)
+                {
+                    returnList.Add(new incidenceVector(inputList[i].term));
+                }
+                returnList[returnList.Count - 1].pageIDs.Add(inputList[i].pageID);
+            }
+            return returnList;
+        }
+    }
+}
