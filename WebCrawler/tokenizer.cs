7b47d4bc59d2b657aeef512eb99ca28f3482a0d4
Kasper Plejdrup <kplejd11@student.aau.dk>
2014-09-17 13:47:27 +0200

    Added Stopwords list

--- WebCrawler/tokenizer.cs
+++ WebCrawler/tokenizer.cs
@@ -27,7 +27,14 @@ namespace SearchEngine
 
         internal static string[] noStopWords(string[] tokens)
         {
-            string[] stopWords = {"is", "a"};
+            string[] stopWords = {"a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "arent", "as", "at", "be", "because", "been", "before",
+            "being", "below", "between", "both", "but", "by", "cant", "cannot", "could", "couldnt", "did", "didnt", "do", "does", "doesnt", "doing", "dont", "down", "during", "each",
+            "few", "for", "from", "further", "had", "hadnt", "has", "hasnt", "have", "havent", "having", "he", "hed", "hell", "hes", "her", "here", "heres", "hers", "herself", "him",
+            "himself", "his", "how", "hows", "i", "im", "ive", "if", "in", "into", "is", "isnt", "it", "its", "itself", "lets", "me", "more", "most", "mustnt", "my", "myself", "no",
+            "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shant", "she", "shes", "should", "shouldnt",
+            "so", "some", "such", "than", "that", "thats", "the", "their", "theirs", "them", "themselves", "then", "there", "theres", "these", "they", "theyd", "theyll", "theyre", "theyve",
+            "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasnt", "we", "weve", "were", "werent", "what", "whats", "when", "whens", "where", "wheres",
+            "which", "while", "who", "whos", "whom", "why", "whys", "with", "wont", "would", "wouldnt", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself", "yourselves"};
             foreach (string word in stopWords)
                 tokens = tokens.Where(w => w != word).ToArray();
             return tokens;
