5b07946cf6471403a3d0fcb475e4be4e0c01b704
Kasper Plejdrup <kplejd11@student.aau.dk>
2014-09-24 13:50:05 +0200

    IDF class

---
+++ WebCrawler/Ranker/IDF.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngine.Ranker
+{
+    class IDF
+    {
+        public void getIDF(List<string> documents, List<incidenceVector> terms)
+        {
+            double pagecount = documents.Count();
+            double df;
+            foreach (incidenceVector page in terms)
+            {
+                df = page.pageIDs.Count();
+                page.idf = (float)Math.Log10(pagecount / df);
+            }
+        }
+    }
+}
