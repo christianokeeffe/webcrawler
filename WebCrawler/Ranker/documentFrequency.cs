c54ce84ba61dd25623ca5310db2e4fc1dbdd6ec1
Mette Pedersen <mettemuzen@hotmail.com>
2014-09-24 14:00:33 +0200

    Made function to calculate TF IDF

--- WebCrawler/Ranker/documentFrequency.cs
+++ WebCrawler/Ranker/documentFrequency.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace SearchEngine.Ranker
 {
-    class tfStar
+    class documentFrequency
     {
-        public void calculateTFStar(List<incidenceVector> listOfTerms) {
+        public void calculateTFAndIDF(List<string> documents, List<incidenceVector> listOfTerms) {
+            double pageCount = documents.Count();
             foreach (incidenceVector term in listOfTerms) {
+                term.idf = (float)Math.Log10(pageCount / term.pageIDs.Count());
                 foreach (Posting post in term.pageIDs) {
                     if (post.pageCount > 0) {
                         post.tfStar = (1 + (float)Math.Log10(post.pageCount));
