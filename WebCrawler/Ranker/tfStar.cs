9e19f85e7d9d36fd091129649209b01c38c59de4
Mette Pedersen <mettemuzen@hotmail.com>
2014-09-24 13:19:02 +0200

    Made tfstar

---
+++ WebCrawler/Ranker/tfStar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchEngine.Ranker
+{
+    class tfStar
+    {
+        public void calculateTFStar(List<incidenceVector> listOfTerms) {
+            foreach (incidenceVector term in listOfTerms) {
+                foreach (Posting post in term.pageIDs) {
+                    if (post.pageCount > 0) {
+                        post.tfStar = (1 + (float)Math.Log10(post.pageCount));
+                    } else {
+                        post.tfStar = 0;
+                    }
+                }
+            }
+        }
+    }
+}
